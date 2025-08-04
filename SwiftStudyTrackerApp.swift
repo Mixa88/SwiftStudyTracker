cbb19c5c94b41557e92d36a5e636eb32ba911e1c
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-04 13:40:11 +0300

    feat: Create Tag model

--- SwiftStudyTrackerApp.swift
+++ SwiftStudyTrackerApp.swift
@@ -14,6 +14,6 @@ struct SwiftStudyTrackerApp: App {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: StudyEntry.self)
+        .modelContainer(for: [StudyEntry.self, Tag.self])
     }
 }
